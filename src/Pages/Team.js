4eaec772601b26b2a100a6956f205e91f3a23c6f
Pétala Leite <petalaleite@gmail.com>
2025-11-26 09:11:05 -0300

    adiciona novo assessor

--- src/Pages/Team.js
+++ src/Pages/Team.js
@@ -10,6 +10,7 @@ function Team() {
         <Container className="py-5">
           <h1>Assessores de Investimentos</h1>
           <p>Wagner Mendonça Costa - CPF 155.502.906-22</p>
+          <p>Filipe Almeida Vasconcellos - CPF 107.505.116-99</p>
         </Container>
       </main>
       <Footer />
